63d6e526dc904c05f41c34451e3bdfe89f11b11c
Chris Wilding <hello@chriswilding.co.uk>
2021-05-02 16:42:36 +0100

    Add gains

---
+++ grains/grains.ts
@@ -0,0 +1,11 @@
+export default class Grains {
+    public static square(n: number): number {
+        if (n < 1 || n > 64) throw new Error()
+
+        return Math.pow(2, n - 1)
+    }
+
+    public static total(): number {
+        return 18446744073709551615
+    }
+}
